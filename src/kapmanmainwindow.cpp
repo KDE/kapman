4c5d88b5ebf8aa68109dff3c17f540fc8b34be20
Laurent Montel <montel@kde.org>
2021-08-30 13:46:49 +0200

    GIT_SILENT: we can use qOverload directly

--- src/kapmanmainwindow.cpp
+++ src/kapmanmainwindow.cpp
@@ -90,7 +90,7 @@ void KapmanMainWindow::initGame()
     m_view->setFocus();
     // For some reason, calling setFocus() immediately won't work after the
     // score dialog has been shown, so do it again after an eventloop run.
-    QTimer::singleShot(0, m_view, QOverload<>::of(&QWidget::setFocus));
+    QTimer::singleShot(0, m_view, qOverload<>(&QWidget::setFocus));
     resetStatusBar();
 }
 
